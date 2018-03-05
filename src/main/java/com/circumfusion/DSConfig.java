2881debadbceaca8d31996e3e74e33b753c93945
Senthilkumar Panneerselvam <senthilapr16@gmail.com>
2018-03-06 00:28:11 +0530

    DS config

---
+++ src/main/java/com/circumfusion/DSConfig.java
@@ -0,0 +1,31 @@
+package com.circumfusion;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+@Configuration
+public class DSConfig {
+	
+
+	    @Bean
+	    public MysqlDataSource dataSource() throws URISyntaxException {
+	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	        String username = dbUri.getUserInfo().split(":")[0];
+	        String password = dbUri.getUserInfo().split(":")[1];
+	        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+	        MysqlDataSource basicDataSource = new MysqlDataSource();
+	        basicDataSource.setUrl(dbUrl);
+	        basicDataSource.setUser(username);
+	        basicDataSource.setPassword(password);
+
+	        return basicDataSource;
+	    }
+
+}
