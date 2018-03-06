7c9e48ada1db6c1ac85f926c470c0b2d7a56aea7
Senthilkumar Panneerselvam <senthilapr16@gmail.com>
2018-03-06 20:28:45 +0530

    config

--- src/main/java/com/circumfusion/DSConfig.java
+++ src/main/java/com/circumfusion/DSConfig.java
@@ -14,8 +14,7 @@ public class DSConfig {
 
 	    @Bean
 	    public MysqlDataSource dataSource() throws URISyntaxException {
-	        System.out.println(System.getenv("DATABASE_URL"));
-	    		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	    		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 
 	        String username = dbUri.getUserInfo().split(":")[0];
 	        String password = dbUri.getUserInfo().split(":")[1];
@@ -32,15 +31,4 @@ public class DSConfig {
 	        return basicDataSource;
 	    }
 	    
-	    public static void main(String[] args) throws URISyntaxException {
-    		URI dbUri = new URI("mysql://circumfusion:a$pire123@circumfusion.c8zaohug6wm6.us-east-2.rds.amazonaws.com:3306/circumfusion");
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(dbUrl);
-	    }
-
 }
